c6aa61883bf1f5300c03b113c5921f304c314793
khaihd <hoangdangkhai.dev@gmail.com>
2024-12-06 13:16:57 +0700

    fix cors url bug

--- src/main/java/com/khaikin/airline/config/SecurityConfiguration.java
+++ src/main/java/com/khaikin/airline/config/SecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -50,9 +51,10 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Chỉ định các nguồn gốc được phép
+//        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Chỉ định các nguồn gốc được phép
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
